7b3942bb4cf447904ffa2a997605f42b292d5d24
Arec <jamgochian95@gmail.com>
2021-07-12 06:23:27 -0700

    adding functions to count collisions in a state or trajectory, check if there are collisions, etc

--- intersim/collisions.py
+++ intersim/collisions.py
@@ -40,31 +40,69 @@ def states_to_polygons(x, lengths, widths):
     inds = torch.where(nni)[0]
     return vp, inds
 
-
-
-def count_collisions(x, lengths, widths):
+def count_collisions_trajectory(x, lengths, widths):
     """
     Count collisions in a trajectory.
     Args:
-        x (torch.tensor): (T,nv*5) vehicle state
-        legnths (torch.tensor): (nv,) vehicle lengths
+        x (torch.tensor): (T,nv,5) vehicle states
+        lengths (torch.tensor): (nv,) vehicle lengths
         widths (torch.tensor): (nv,) vehicle lengths
     Returns
         ncols (int): number of collisions
     """
 
-    T,_ = x.shape
-    x = x.reshape(T, -1, 5)
-    nv0 = x.shape[1]
+    T,nv0,_ = x.shape
     cols = torch.zeros((nv0, nv0))
     for t in range(T):
-        polys_t, nni_t = states_to_polygons(x[t], lengths, widths)
-        nv = len(polys_t)
-        for i in range(1,nv):
-            for j in range(i):
-                if polys_t[i].intersects(polys_t[j]):
-                    cols[nni_t[i],nni_t[j]] += 1
-                    
-    cols = cols + cols.T # make symmetric 
+        cols += collision_matrix(x[t], lengths, widths)
     ncols = np.count_nonzero(cols) / 2 # divide by 2 since symmetric
     return ncols
+
+def count_collisions(x, lengths, widths):
+    """
+    Count collisions in a single frame.
+    Args:
+        x (torch.tensor): (nv,5) vehicle states
+        lengths (torch.tensor): (nv,) vehicle lengths
+        widths (torch.tensor): (nv,) vehicle lengths
+    Returns
+        ncols (int): number of collisions
+    """
+
+    T,nv0,_ = x.shape
+    cols = collision_matrix(x, lengths, widths)
+    ncols = np.count_nonzero(cols) / 2 # divide by 2 since symmetric
+    return ncols
+
+def check_collisions(x, length, widths)
+    """
+    Check whether there are collisions in a single frame.
+    Args:
+        x (torch.tensor): (nv,5) vehicle states
+        lengths (torch.tensor): (nv,) vehicle lengths
+        widths (torch.tensor): (nv,) vehicle lengths
+    Returns
+        col (bool): True if there are collisions
+    """
+    return count_collisions(x, lengths, widths) > 0
+
+def collision_matrix(x, lengths, widths):
+    """
+    Generate collision matrix at a state.
+    Args:
+        x (torch.tensor): (nv,5) vehicle state
+        lengths (torch.tensor): (nv,) vehicle lengths
+        widths (torch.tensor): (nv,) vehicle lengths
+    Returns
+        c_mat (torch.tensor): (nv, nv) collision tensor 
+            where [i,j] is True if i is colliding with j
+    """
+    polys_t, nni_t = states_to_polygons(x, lengths, widths)
+    nv = len(polys_t)
+    c_mat = torch.zeros(nv, nv)
+    for i in range(1,nv):
+        for j in range(i):
+            if polys_t[i].intersects(polys_t[j]):
+                c_mat[nni_t[i],nni_t[j]] = 1
+                c_mat[nni_t[j],nni_t[i]] = 1
+    return c_mat
