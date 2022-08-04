fix overflow in eval_poly

intersim/envs/simulator_jax.py
@@ -23,4 +23,17 @@ def generate_paths(state, ds, xpoly, ypoly, dxpoly, dypoly, smax):
     return x, y
 
 def eval_poly(s, poly):
-    return (poly[..., jnp.newaxis, :] * jnp.power(s[..., jnp.newaxis], jnp.arange(poly.shape[-1]))).sum(-1)
+    coeffs = poly[..., jnp.newaxis, :]
+    bases = s[..., :, jnp.newaxis]
+    lg_abs_coeffs = jnp.log(jnp.abs(coeffs))
+    lg_abs_powers = jnp.log(jnp.abs(bases)) * jnp.arange(poly.shape[-1])
+
+    coeffs_pos = coeffs >= 0
+    even_powers = jnp.ones_like(lg_abs_powers).at[..., jnp.arange(1, poly.shape[-1], 2)].set(0)
+    powers_pos = jnp.logical_or(bases >= 0, even_powers)
+    
+    coeffs_sgn = -1 + 2 * coeffs_pos
+    powers_sgn = -1 + 2 * powers_pos
+    sgn = coeffs_sgn * powers_sgn
+
+    return (sgn * jnp.exp(lg_abs_coeffs + lg_abs_powers)).sum(-1)

tests/intersim/envs/test_intersimple.py
@@ -1,8 +1,8 @@
-from intersim.envs import Intersimple, NRasterized
+from intersim.envs import Intersimple, NRasterizedRoute
 import numpy as np
 import logging
 import pytest
-
+import torch
 
 def test_init_runs():
     env = Intersimple()
@@ -214,3 +214,27 @@ def test_reset_equals_playback():
     assert (env._env._exceeded == env_skip._env._exceeded).all()
     # comparison not implemented for InteractionGraph, skipping ._env.graph
     assert np.allclose(env._env.info['raw_state'], env_skip._env.info['raw_state'], atol=0.20, equal_nan=True)
+
+def test_generate_paths():
+    wenv = NRasterizedRoute()
+    wenv._agent = 76
+    wenv.reset()
+    env = wenv._env
+
+    smax = env.smax[0]
+    n = 20
+
+    xpath, ypath = env._generate_paths(delta=2*smax/n, n=n, is_distance=True, override=False)
+    # env.state will be nan for all tracks not visible in the current state
+    existing_tracks = ~torch.any(torch.isnan(env.state), dim=1)
+    xpath, ypath = xpath[existing_tracks], ypath[existing_tracks]
+    assert ~torch.any(torch.isnan(xpath))
+    assert ~torch.any(torch.isnan(ypath))
+
+    v0 = env.state[76, 1]
+    xpath, ypath = env._generate_paths(delta=2*smax/(v0*n), n=n, is_distance=False, override=False)
+    # env.state will be nan for all tracks not visible in the current state
+    existing_tracks = ~torch.any(torch.isnan(env.state), dim=1)
+    xpath, ypath = xpath[existing_tracks], ypath[existing_tracks]
+    assert ~torch.any(torch.isnan(xpath))
+    assert ~torch.any(torch.isnan(ypath))